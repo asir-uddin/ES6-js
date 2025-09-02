a832baaf9d4851f63853f52ff94d4674bd9981db
Asir <mdasir017@gmaol.com>
2025-09-03 00:55:50 +0600

    added my default paramitter and template string

---
+++ default-paramiter.js
@@ -0,0 +1,27 @@
+// function sum(num1, num2){
+//     sum = num1 + num2;
+//     console.log(sum);
+// }
+// sum(23, 34);
+
+// function sum(num1, num2 = 0){
+//     sum = num1 + num2;
+//     console.log(num1, num2, sum);
+// }
+// sum(23);
+
+
+
+
+// multiply
+// function multiply(num1, num2){
+//     sum = num1 * num2;
+//     console.log(sum);
+// }
+// multiply(23, 34);
+
+function multiply(num1, num2 = 1){
+    sum = num1 * num2;
+    console.log(sum);
+}
+multiply(23);
---
+++ template-string.js
@@ -0,0 +1,23 @@
+// const poem = 'hello i am a web developer on full stack 
+
+// const poem = 'sofik ia plumbing \n'+
+// ' boy his name\n'+
+//   'is same a';
+//   console.log(poem)
+
+const poem =`hello dear student how are you?
+I hope you will be happy 
+So, I can do my class today 
+definitely you are a very important person`
+// console.log(poem)
+
+// function sum(num1, num2){
+//     const result = num1 + num2;
+//     const outPut = `sum of ${num1} and ${num2} is equal ${result}`
+//     console.log(outPut)
+// }
+// sum(10, 20);
+
+const price = 50;
+const discount = `Discounted price is ${price} after 20% discount ${price* 0.8}`;
+console.log(discount);
